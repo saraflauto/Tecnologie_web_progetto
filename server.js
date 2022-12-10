6f45952146c4c4b62c8445f559d157ac19a2ab56
saraflauto <74247221+saraflauto@users.noreply.github.com>
2022-12-10 20:19:21 +0100

    Update server.js

--- server.js
+++ server.js
@@ -26,6 +26,54 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //css
 app.use(express.static("stili"));
 
+//Login google
+const session = require('express-session');
+const passport = require('passport');
+require('./auth');
+
+//id dell'account google
+const id = " ";
+//nome dell'account google
+const fullname = " ";
+function isLoggedIn(req, res, next) {
+   req.user ? next() : res.sendStatus(401);
+ }
+ 
+ app.use(session({ secret: 'cats', resave: false, saveUninitialized: true }));
+ app.use(passport.initialize());
+ app.use(passport.session());
+ 
+ app.get('/', (req, res) => {
+   res.send('<a href="/auth/google">Authenticate with Google</a>');
+ });
+ 
+ app.get('/auth/google',
+   passport.authenticate('google', { scope: [ 'email', 'profile' ] }
+ ));
+ 
+ app.get( '/auth/google/callback',
+   passport.authenticate( 'google', {
+     successRedirect: '/getinfo',
+     failureRedirect: '/auth/google/failure'
+   })
+ );
+ 
+ app.get('/getinfo', isLoggedIn, (req, res) => {
+   id = req.user.id;
+   fullname = req.user.displayName;
+   res.redirect('/home');
+ });
+ 
+ app.get('/logout', (req, res) => {
+   req.logout();
+   req.session.destroy();
+   res.redirect('/');
+ });
+ 
+ app.get('/auth/google/failure', (req, res) => {
+   res.send('Failed to authenticate..');
+ });
+
 
 //home page
 app.get("/home", function (req, res) {
